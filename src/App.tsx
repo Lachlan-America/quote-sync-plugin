019ab0f244134a44630405be3f48d77470761369
Captain. A <loch.america112@gmail.com>
2025-06-16 16:04:50 +1000

    Plugin for Insert Frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,69 @@
-import { framer, CanvasNode } from "framer-plugin"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { framer, useIsAllowedTo } from "framer-plugin"
 import "./App.css"
 
+// Show how the UI should be displayed in Framer
 framer.showUI({
-    position: "top right",
-    width: 240,
-    height: 95,
+    position: "center",
+    width: 300,
+    height: 200,
 })
 
-function useSelection() {
-    const [selection, setSelection] = useState<CanvasNode[]>([])
-
-    useEffect(() => {
-        return framer.subscribeToSelection(setSelection)
-    }, [])
-
-    return selection
-}
-
+/* The code for the main plugin */
 export function App() {
-    const selection = useSelection()
-    const layer = selection.length === 1 ? "layer" : "layers"
+    // Need to check permissions before the plugin can access pluginData
+    const isAllowed = useIsAllowedTo("setPluginData", "addComponentInstance")
+    // This is used to determine whether to show the author in the quote component
+    const [authorToggle, setAuthorToggle] = useState<boolean>(false)
 
-    const handleAddSvg = async () => {
-        await framer.addSVG({
-            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="#999" d="M20 0v8h-8L4 0ZM4 8h8l8 8h-8v8l-8-8Z"/></svg>`,
-            name: "Logo.svg",
+    /*
+    * This function fetches a random quote from The Quotes Hub API and instantiates a Framer component with the quote data.
+    */
+    const handleSyncQuote = async () => {
+        const res = await fetch("https://thequoteshub.com/api/")
+        const data = await res.json()
+        // Check if the data is valid
+        if (!data || !data.text) {
+            console.error("Failed to fetch quote data.")
+            return
+        }
+        console.log(data)
+
+        // Have to ensure permissions are granted before using the following methods
+        if (isAllowed) {
+            // Set the storage keys to the main quote and author
+            await framer.setPluginData("quote", data.text)
+            await framer.setPluginData("author", data.author)
+
+            await framer.addComponentInstance({ url: "https://framer.com/m/Quote-3rGY.js@i1HEx1bL1u0Ctf1SMxfu",
+            attributes: {
+                // This is how to pass data to the component
+                controls: {
+                    content: data.text, 
+                    author: data.author, 
+                    authorToggle: authorToggle
+                },
+            }
         })
+        } else {
+            console.warn("Not allowed to access pluginData.")
+        }
     }
 
     return (
         <main>
-            <p>
-                Welcome! Check out the{" "}
-                <a href="https://framer.com/developers/plugins/introduction" target="_blank">
-                    Docs
-                </a>{" "}
-                to start. You have {selection.length} {layer} selected.
+            <p style={{ marginTop: 8, fontSize: 12 }}>
+                Click the button below to sync a random quote from The Quotes Hub API.
             </p>
-            <button className="framer-button-primary" onClick={handleAddSvg}>
-                Insert Logo
+            <div style={{ alignItems: "center", display: "flex" }}>
+                <label style={{ paddingRight: 5, fontSize: 12 }}>Enable Author:</label>
+                <label className="switch">
+                    <input type="checkbox" checked={authorToggle} onChange={() => setAuthorToggle(!authorToggle)} />
+                    <span className="slider round"></span>
+                </label>
+            </div>
+            <button className="framer-button-primary" onClick={handleSyncQuote}>
+                Sync Quote
             </button>
         </main>
     )
